1a8db0d4e3120567722e1d50f30d0ef425b08b5a
rahilsyed <rahilcloud08@gmail.com>
2025-07-29 15:00:09 +0530

    add typeDef

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,16 +2,17 @@ import { prismaClient } from '../lib/db';
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { User } from './user';
+import { typeDefs } from './user/typeDef';
 async function createApolloServer() {
   const gqlServer = new ApolloServer({
     typeDefs: `
-    
+    ${typeDefs}
       type Query {
         ${User.queries}
       }
-        type Mutation {
-        ${User.mutations}
-        }
+      type Mutation {
+      ${User.mutations}
+      }
     `,
     resolvers: {
       Query: { ...User.resolvers.Queries },
